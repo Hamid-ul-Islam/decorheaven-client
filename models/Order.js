b51acec2857939eb7d3e184739b118090c845522
Hamidul Islam <hamidthedev@gmail.com>
2024-04-25 11:21:24 +0600

    before keep design installed

---
+++ models/Order.js
@@ -0,0 +1,17 @@
+import {model, models, Schema} from "mongoose";
+
+const OrderSchema = new Schema({
+  line_items:Object,
+  name:String,
+  email:String,
+  phone: String,
+  total:Number,
+  city: String,
+  postalCode:String,
+  streetAddress:String,
+  paid:Boolean,
+}, {
+  timestamps: true,
+});
+
+export const Order = models?.Order || model('Order', OrderSchema);
